5dd28bb2ba22be600d5d1373ebf6e2509c310b8c
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-03-03 03:20:22 -0500

    implemented Website and Link POJO. closes #2

--- src/main/java/model/Website.java
+++ src/main/java/model/Website.java
@@ -4,10 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * websiteURL is the total URL link
+ * linksList is the set of all the links present in a website/document(JSoup)
+ * topKeyWords is the dictionary count of all the words in the document, might be based on some filter
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Website {
-	private String location;
-	private int numberOfHits;
+	private String websiteURL;
+	private Set<Link> linksList;
+	private Map<String, Integer> topKeyWords;
 }
